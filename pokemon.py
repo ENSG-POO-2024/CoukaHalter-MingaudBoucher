619c7e069c082c2de739ee8f7aecbefcf93ba429
Antoine Halter-Mingaud <antoine.haltermingaud@gmail.com>
2024-05-03 12:08:43 +0200

    création classes de pokemon

---
+++ pokemon.py
@@ -0,0 +1,57 @@
+from abc import ABCMeta, abstractmethod, ABC
+
+
+class pokemon(metaclass=ABCMeta):
+    def __init__(
+        self, nom, type1, hp, atk, defense, atk_spe, defense_spe, vitesse, legendaire
+    ) -> None:
+        self.nom = nom
+        self.type1 = type1
+        self.hp = hp
+        self.atk = atk
+        self.defense = defense
+        self.atk_spe = atk_spe
+        self.defense_spe = defense_spe
+        self.vitesse = vitesse
+        self.legendaire = legendaire
+
+    @abstractmethod
+    def attributs(self):
+        afficher_attributs = f"Nom : {self.nom}\nType1 : {self.type1}\nHP : {self.hp}\nAttaque : {self.atk}\nDefense : {self.defense}\nAttaque speciale : {self.atk_spe}\nVitesse : {self.vitesse}\nLegendaire : {self.legendaire}"
+        print(afficher_attributs)
+
+
+class pokemonSauvage(pokemon):
+    def __init__(
+        self,
+        nom,
+        type1,
+        hp,
+        atk,
+        defense,
+        atk_spe,
+        defense_spe,
+        vitesse,
+        legendaire,
+        position,
+    ) -> None:
+        super().__init__(
+            nom, type1, hp, atk, defense, atk_spe, defense_spe, vitesse, legendaire
+        )
+        self.position = position
+
+    def attributs(self):
+        super().attributs()
+        print(f"Position : {self.position}")
+
+
+class pokemonCapture(pokemon):
+    def __init__(
+        self, nom, type1, hp, atk, defense, atk_spe, defense_spe, vitesse, legendaire
+    ) -> None:
+        super().__init__(
+            nom, type1, hp, atk, defense, atk_spe, defense_spe, vitesse, legendaire
+        )
+
+    def attributs(self):
+        return super().attributs()
