0e584eaa4e95b51a2238869949a6911044074a94
gilcka <gilcka05@gmail.com>
2024-05-06 16:14:36 +0200

    rename des sprites

--- rename.py
+++ rename.py
@@ -1,21 +1,12 @@
 import os
 import pandas as pd
-import numpy as np
 
-repertoire = 'assets'
-nouveau_repertoire = 'sprites'
+repertoire = 'assets/frame2'
 noms = pd.read_csv('data/pokemon_first_gen.csv')
 nomsNumpy = noms.values
 
-for index, nom_fichier in enumerate(os.listdir(repertoire)):
-    # Générer le nouveau nom de fichier
-    nouveau_nom_fichier = noms[index, 1]
+for i in range (151):
     
-    # Ancien chemin complet du fichier
-    ancien_chemin = os.path.join(repertoire, nom_fichier)
+    nouveau_nom_fichier = f"sprites/back/frame2/{nomsNumpy[i, 1]}.png"
     
-    # Nouveau chemin complet du fichier
-    nouveau_chemin = os.path.join(nouveau_repertoire, nouveau_nom_fichier)
-    
-    # Renommer le fichier
-    os.rename(ancien_chemin, nouveau_chemin)
+    os.rename(f"assets/frame2/{i+1}.png", nouveau_nom_fichier)
