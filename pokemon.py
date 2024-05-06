7d7dd66591019eff047f2d7cc5a9747e1a56bc55
Antoine Halter-Mingaud <antoine.haltermingaud@gmail.com>
2024-05-06 16:50:00 +0200

    Résolu import circulaire

--- pokemon.py
+++ pokemon.py
@@ -1,21 +1,20 @@
 from abc import ABCMeta, abstractmethod, ABC
-from joueur import joueur
 
 
 class pokemon(metaclass=ABCMeta):
     def __init__(
-        self, 
-        nom, 
-        type1, 
-        hp, 
-        atk, 
-        defense, 
-        atk_spe, 
-        defense_spe, 
-        vitesse, 
+        self,
+        nom,
+        type1,
+        hp,
+        atk,
+        defense,
+        atk_spe,
+        defense_spe,
+        vitesse,
         legendaire,
     ) -> None:
-        
+
         self.nom = nom
         self.type1 = type1
         self.hp = hp
@@ -28,13 +27,13 @@ class pokemon(metaclass=ABCMeta):
 
     @abstractmethod
     def attributs(self):
-        
+
         afficher_attributs = f"Nom : {self.nom}\nType1 : {self.type1}\nHP : {self.hp}\nAttaque : {self.atk}\nDefense : {self.defense}\nAttaque speciale : {self.atk_spe}\nVitesse : {self.vitesse}\nLegendaire : {self.legendaire}"
         print(afficher_attributs)
 
 
 class pokemonSauvage(pokemon):
-    def __init__(            
+    def __init__(
         self,
         nom,
         type1,
@@ -47,29 +46,38 @@ class pokemonSauvage(pokemon):
         legendaire,
         position,
     ) -> None:
-        
+
         super().__init__(
             nom, type1, hp, atk, defense, atk_spe, defense_spe, vitesse, legendaire
         )
         self.position = position
 
     def attributs(self):
-        
+
         super().attributs()
         print(f"Position : {self.position}")
 
 
 class pokemonCapture(pokemon):
-    
+
     def __init__(
-        self, ID, nom, type1, hp, atk, defense, atk_spe, defense_spe, vitesse, legendaire
+        self,
+        ID,
+        nom,
+        type1,
+        hp,
+        atk,
+        defense,
+        atk_spe,
+        defense_spe,
+        vitesse,
+        legendaire,
     ) -> None:
         super().__init__(
             nom, type1, hp, atk, defense, atk_spe, defense_spe, vitesse, legendaire
         )
         self.ID = ID
 
-    
     def attributs(self):
-        
+
         return super().attributs()
