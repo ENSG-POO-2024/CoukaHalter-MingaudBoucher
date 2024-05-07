b3935ec23d4aafba4af4d0ff7dc5ecbe6a06305a
Antoine Halter-Mingaud <antoine.haltermingaud@gmail.com>
2024-05-07 22:54:00 +0200

    modification déplacement naïf de la carte

--- interface/game_window.py
+++ interface/game_window.py
@@ -8,14 +8,14 @@ import sys
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.setFixedSize(800, 600)  # Set fixed size for the window
+        MainWindow.setFixedSize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.map_label = QtWidgets.QLabel(self.centralwidget)
-        self.map_label.setGeometry(QtCore.QRect(0, 0, 800, 600))  # Set map size
+        self.map_label.setGeometry(QtCore.QRect(0, 0, 800, 600))
         self.character_label = QtWidgets.QLabel(self.centralwidget)
         self.character_label.setGeometry(
-            QtCore.QRect(400, 300, 20, 20)
+            QtCore.QRect(390, 290, 20, 20)
         )  # Center of the screen
         self.character_label.setStyleSheet(
             "background-color: red; border-radius: 10px"
@@ -41,54 +41,50 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.map_label.setPixmap(QtGui.QPixmap("./assets/map.jpg"))  # Set map image
-        self.map_label.setScaledContents(True)  # Scale the image to fit the label
+        self.map_pixmap = QtGui.QPixmap("./assets/map.jpg")
+        self.map_label.setPixmap(self.map_pixmap)
+        self.map_label.setScaledContents(True)
 
-        self.map_width = 600  # Map width
-        self.map_height = 400  # Map height
-        self.character_width = 20  # Character width
-        self.character_height = 20  # Character height
+        self.map_width = self.map_pixmap.width()
+        self.map_height = self.map_pixmap.height()
 
-        self.window_width = self.map_width
-        self.window_height = self.map_height
-
-        self.center_character()
+        # Initial window size and position
+        self.window_width = 400
+        self.window_height = 300
+        self.window_x = 0
+        self.window_y = 0
 
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_Left:
-            self.moveMap(10, 0)  # Move map left
+            self.moveMap(-10, 0)
         elif event.key() == QtCore.Qt.Key_Right:
-            self.moveMap(-10, 0)  # Move map right
+            self.moveMap(10, 0)
         elif event.key() == QtCore.Qt.Key_Up:
-            self.moveMap(0, 10)  # Move map up
+            self.moveMap(0, -10)
         elif event.key() == QtCore.Qt.Key_Down:
-            self.moveMap(0, -10)  # Move map down
+            self.moveMap(0, 10)
 
     def moveMap(self, dx, dy):
-        new_map_x = self.map_label.x() + dx
-        new_map_y = self.map_label.y() + dy
-        new_character_x = self.character_label.x() - dx
-        new_character_y = self.character_label.y() - dy
+        self.window_x += dx
+        self.window_y += dy
 
-        # Adjust map position based on character movement
-        if new_map_x < 0:
-            new_map_x = 0
-        elif new_map_x > self.map_width - self.window_width:
-            new_map_x = self.map_width - self.window_width
+        if self.window_x < 0:
+            self.window_x = 0
+        elif self.window_x > self.map_width - self.window_width:
+            self.window_x = self.map_width - self.window_width
 
-        if new_map_y < 0:
-            new_map_y = 0
-        elif new_map_y > self.map_height - self.window_height:
-            new_map_y = self.map_height - self.window_height
+        if self.window_y < 0:
+            self.window_y = 0
+        elif self.window_y > self.map_height - self.window_height:
+            self.window_y = self.map_height - self.window_height
 
-        self.map_label.move(new_map_x, new_map_y)
-        self.character_label.move(new_character_x, new_character_y)
+        self.updateMap()
 
-    def center_character(self):
-        self.character_label.move(
-            self.window_width // 2 - self.character_label.width() // 2,
-            self.window_height // 2 - self.character_label.height() // 2,
+    def updateMap(self):
+        cropped_pixmap = self.map_pixmap.copy(
+            self.window_x, self.window_y, self.window_width, self.window_height
         )
+        self.map_label.setPixmap(cropped_pixmap)
 
 
 if __name__ == "__main__":
