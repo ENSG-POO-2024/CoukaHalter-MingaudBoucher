c1ac3e9a89f1aeab0599f213466caeba91458520
Antoine Halter-Mingaud <antoine.haltermingaud@gmail.com>
2024-05-15 16:27:20 +0200

    ajout signatures de fonctions

--- interface/game_window.py
+++ interface/game_window.py
@@ -7,7 +7,7 @@ import math
 
 
 class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
+    def setupUi(self, MainWindow: QMainWindow) -> None:
         MainWindow.setObjectName("MainWindow")
         self.window_size_x = 800
         self.window_size_y = 600
@@ -23,13 +23,13 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self, MainWindow: QMainWindow) -> None:
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Pokemon Python"))
 
 
 class PointWindow(QMainWindow):
-    def __init__(self, main_window):
+    def __init__(self, main_window: "MainWindow") -> None:
         super().__init__()
         self.main_window = main_window
         self.setGeometry(100, 100, 600, 600)
@@ -38,8 +38,7 @@ class PointWindow(QMainWindow):
         self.label.setGeometry(50, 50, 200, 200)
         self.label.setText("You are near a point!")
 
-    def closeEvent(self, event):
-        # Call the MainWindow method to resume background music
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
         self.main_window.resumeBackgroundMusic()
         super().closeEvent(event)
 
@@ -54,7 +53,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         "right": [f"./sprites/front/trainer/tile{i}.png" for i in range(102, 105)],
     }
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
         self.loadMap()
@@ -70,11 +69,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.current_frame = 0
         self.direction = ""
         self.is_walking = False
-        self.points = []  # List to store the points
-        self.point_windows = []  # List to store opened point windows
+        self.points = []
+        self.point_windows = []
         self.generateRandomPoints()
 
-    def loadMap(self):
+    def loadMap(self) -> None:
         self.map_pixmap = QtGui.QPixmap(self.MAP_FILE)
         self.map_label.setPixmap(self.map_pixmap)
         self.map_label.setScaledContents(True)
@@ -83,26 +82,26 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.map_pixmap.height(),
         )
 
-    def loadCharacter(self):
+    def loadCharacter(self) -> None:
         character_image = QtGui.QPixmap("./sprites/front/trainer/tile102.png")
         self.character_label.setPixmap(character_image)
         self.character_label.setGeometry(self.centerCharacter(character_image))
 
-    def centerCharacter(self, character_image):
+    def centerCharacter(self, character_image: QtGui.QPixmap) -> QtCore.QRect:
         center_x = (self.window_size_x - character_image.width()) // 2
         center_y = (self.window_size_y - character_image.height()) // 2
         return QtCore.QRect(
             center_x, center_y, character_image.width(), character_image.height()
         )
 
-    def initSounds(self):
+    def initSounds(self) -> None:
         self.walk_sound = QSoundEffect()
         self.walk_sound.setSource(
             QtCore.QUrl.fromLocalFile("./assets/sounds/footsteps.wav")
         )
         self.walk_sound.setVolume(0.5)
 
-    def initBackgroundMusic(self):
+    def initBackgroundMusic(self) -> None:
         self.background_music = QSoundEffect()
         self.background_music.setSource(
             QtCore.QUrl.fromLocalFile("./assets/sounds/background.wav")
@@ -111,7 +110,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.background_music.setVolume(0.5)
         self.background_music.play()
 
-    def keyPressEvent(self, event):
+    def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
         if event.isAutoRepeat():
             return
         if event.key() == QtCore.Qt.Key_Left:
@@ -123,7 +122,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         elif event.key() == QtCore.Qt.Key_Down:
             self.walkDown()
 
-    def keyReleaseEvent(self, event):
+    def keyReleaseEvent(self, event: QtGui.QKeyEvent) -> None:
         if event.isAutoRepeat():
             return
         if event.key() in {
@@ -134,7 +133,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         }:
             self.stopWalking()
 
-    def moveMap(self, dx, dy):
+    def moveMap(self, dx: int, dy: int) -> None:
         self.window_x += dx
         self.window_y += dy
         self.window_x = max(0, min(self.window_x, self.map_width - self.window_size_x))
@@ -142,33 +141,24 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.updateMap()
         self.updatePoints()
 
-    def moveCharacter(self, dx, dy):
+    def moveCharacter(self, dx: int, dy: int) -> None:
         new_x = self.character_x + dx
         new_y = self.character_y + dy
-
-        if new_x < 0:
-            new_x = 0
-        elif new_x > self.map_width - self.character_label.width():
-            new_x = self.map_width - self.character_label.width()
-
-        if new_y < 0:
-            new_y = 0
-        elif new_y > self.map_height - self.character_label.height():
-            new_y = self.map_height - self.character_label.height()
-
+        new_x = max(0, min(new_x, self.map_width - self.character_label.width()))
+        new_y = max(0, min(new_y, self.map_height - self.character_label.height()))
         self.character_x = new_x
         self.character_y = new_y
         self.character_label.move(
             self.character_x - self.window_x, self.character_y - self.window_y
         )
 
-    def updateMap(self):
+    def updateMap(self) -> None:
         cropped_pixmap = self.map_pixmap.copy(
             self.window_x, self.window_y, self.window_size_x, self.window_size_y
         )
         self.map_label.setPixmap(cropped_pixmap)
 
-    def walk(self, direction):
+    def walk(self, direction: str) -> None:
         self.is_walking = True
         if not self.walk_sound.isPlaying():
             self.walk_sound.play()
@@ -178,12 +168,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.timer.start(80)
         print(self.character_x, self.character_y)
 
-    def stopWalking(self):
+    def stopWalking(self) -> None:
         self.is_walking = False
         self.timer.stop()
         self.walk_sound.stop()
 
-    def updateWalk(self):
+    def updateWalk(self) -> None:
         if self.current_frame < len(self.walk_frames):
             frame_file = self.walk_frames[self.current_frame]
             frame = QtGui.QPixmap(frame_file)
@@ -200,23 +190,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         else:
             self.current_frame = 0
 
-    def walkDown(self):
+    def walkDown(self) -> None:
         self.walk("down")
 
-    def walkUp(self):
+    def walkUp(self) -> None:
         self.walk("up")
 
-    def walkLeft(self):
+    def walkLeft(self) -> None:
         self.walk("left")
 
-    def walkRight(self):
+    def walkRight(self) -> None:
         self.walk("right")
 
-    def generateRandomPoints(self):
-        for _ in range(10):  # Change the number to generate more or fewer points
-            map_x = random.randint(0, self.map_width - 20)  # Keep map coordinates
+    def generateRandomPoints(self) -> None:
+        for _ in range(10):
+            map_x = random.randint(0, self.map_width - 20)
             map_y = random.randint(0, self.map_height - 20)
-
             window_x = map_x - self.window_x
             window_y = map_y - self.window_y
             point_label = QtWidgets.QLabel(self.centralwidget)
@@ -226,10 +215,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.points.append((map_x, map_y, point_label))
             print(self.map_width, self.map_height)
 
-    def distance(self, x1, y1, x2, y2):
+    def distance(self, x1: float, y1: float, x2: float, y2: float) -> float:
         return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-    def updatePoints(self):
+    def updatePoints(self) -> None:
         character_center_x = self.character_x + self.character_label.width() / 2
         character_center_y = self.character_y + self.character_label.height() / 2
 
@@ -240,27 +229,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             )
 
             if distance_to_point < 100:
-                print(f"Character is near point {idx+1}")
+                print(f"Character is near point {idx + 1}")
                 if idx not in self.point_windows:
                     self.point_windows.append(idx)
                     point_label.hide()
                     self.openPointWindow()
 
-    def openPointWindow(self):
+    def openPointWindow(self) -> None:
         self.background_music.stop()
-
         self.combat_music = QSoundEffect()
         self.combat_music.setSource(
             QtCore.QUrl.fromLocalFile("./assets/sounds/combat_music.wav")
         )
         self.combat_music.setVolume(0.5)
         self.combat_music.play()
-
         self.point_window = PointWindow(self)
         self.stopWalking()
         self.point_window.show()
 
-    def resumeBackgroundMusic(self):
+    def resumeBackgroundMusic(self) -> None:
         self.combat_music.stop()
         self.background_music.play()
 
